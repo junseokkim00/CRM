c0a5ce14eadac037c4476a5b7ddbd73a54ae0e1d
Junseok Kim <kim.junseok@snu.ac.kr>
2025-04-23 15:43:37 +0900

    change requirements.txt

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name generate-crm-data
+#SBATCH --job-name crm-initial-data-verdict
 #SBATCH --gpus 1
 #SBATCH --time 1-00:00:00
-#SBATCH --nodelist a6k01
+#SBATCH --nodelist a5k01
 
 # Junseok Kim
 # python generate_data.py \
 #     --dataset gsm8k \
 #     --num_feedbacks 3 \
 #     --num_revision 4 \
-#     --start 0 \
+#     --start 2355 \
 #     --end 2491
 
 # KyoChul Jang
@@ -28,3 +28,16 @@
 #     --num_revision 4 \
 #     --start 4982 \
 #     --end 7473
+
+
+# python estimate_initial_answer.py \
+#     --dataset gsm8k \
+#     --start 0 \
+#     --end 7473 \
+#     --data_path logs/gsm8k_full.jsonl
+
+python estimate_initial_answer_for_test.py \
+    --dataset gsm8k \
+    --start 0 \
+    --end 7473 \
+    --data_path logs/evaluate_gsm8k.jsonl
